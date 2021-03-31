bbe8b864e7dbcb7460adad05a06d4e2788b9f067
ZhuoqunWang0120 <18805156320@163.com>
2021-03-31 05:15:05 +0000

    unfinished

--- .ipynb_checkpoints/utilities-checkpoint.py
+++ .ipynb_checkpoints/utilities-checkpoint.py
@@ -14,8 +14,8 @@ def parse_doc(doc,vocab):
     doc: document string
     vocab: a dictionary that maps words to integers
     Output:
-    A dictionary, where the keys are words appeared in the doc, labeled with the integers in the vocab dictionary (the set of $\tilde{w_n}$), 
-        and the values are counts of the words.
+    A list of tuples, where for each tuple, the first element is a word appeared in the doc, labeled with the integers in the vocab dictionary (the set of $\tilde{w_n}$), 
+        and the second element is count of the words.
     The words that are not in vocab will be ignored.
     """
     doc=doc.lower()
@@ -28,4 +28,30 @@ def parse_doc(doc,vocab):
     del words_dict[-1] # ignore the words outside the vocabulary
     #wordid=words_dict.keys()
     #wordcnt=words_dict.values()
-    return words_dict
+    return sorted(words_dict.items())
+
+def make_vocab_from_docs(docs):
+    """
+    Make a dictionary that contains all words from the docs. The order of words is arbitrary.
+    docs: iterable of documents
+    """
+    vocab_words=set()
+    for doc in docs:
+        doc=doc.lower()
+        doc=re.sub(r'-',' ',doc)
+        doc=re.sub(r' +',' ',doc) # turn multiple spaces into a single space
+        doc=re.sub(r'[^a-z ]','',doc) # remove anything that is not a-z or space
+        words=set(doc.split())
+        vocab_words=vocab_words.union(words)
+        vocab=dict(zip(vocab_words,range(len(vocab_words))))
+    return vocab
+
+def make_data(docs):
+    """
+    Make the input for variational_em function from docs.
+    """
+    vocab=make_vocab_from_docs(docs)
+    word_dicts=list(map(lambda x: parse_doc(x,vocab),docs))
+    Nd=list(map(len,docs))
+    M,V=len(docs),len(vocab)
+    return (vocab,word_dicts,Nd,M,V)
